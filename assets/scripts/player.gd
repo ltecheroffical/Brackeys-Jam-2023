47768a680654756066602a4231f036a6e276bd8f
Lawrence <82725076+la-jig@users.noreply.github.com>
2023-08-08 17:01:16 -0400

    Updated player

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -4,21 +4,21 @@ const SPEED = 50.0
 const JUMP_VELOCITY = -250.0
 const COYOTE_TIME = 0.2
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var timeSinceLeftGround: float = 0.0
+var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _timeSinceLeftGround: float = 0.0
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += _gravity * delta
 	
 	if is_on_floor():
-		timeSinceLeftGround = 0.0
+		_timeSinceLeftGround = 0.0
 	else:
-		timeSinceLeftGround += delta
+		_timeSinceLeftGround += delta
 
 	if Input.is_action_just_pressed("jump") and can_jump():
 		velocity.y = JUMP_VELOCITY
-		timeSinceLeftGround = 10
+		_timeSinceLeftGround = 10
 
 	var direction = Input.get_axis("left", "right")
 	if direction:
@@ -35,4 +35,4 @@ func _physics_process(delta):
 	move_and_slide()
 
 func can_jump() -> bool:
-	return is_on_floor() or timeSinceLeftGround < COYOTE_TIME
+	return is_on_floor() or _timeSinceLeftGround < COYOTE_TIME
