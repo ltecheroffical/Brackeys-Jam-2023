d95376cab8e72b6c61a365eee22ed6703c3974d8
Cube <cubelogic.games@gmail.com>
2023-08-08 22:40:14 +0300

    Added enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@export var speed := 400
+@onready var sprite := $Sprite2D
+@export var speed := 100
 var direction := 1
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	collide_with_wall()
-	velocity.x = speed * direction * delta
-	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	velocity.x = speed * direction
 	move_and_slide()
 
 func collide_with_wall():
@@ -14,4 +17,5 @@ func collide_with_wall():
 		change_direction()
 
 func change_direction():
+	sprite.flip_h = !sprite.flip_h
 	direction *= -1
