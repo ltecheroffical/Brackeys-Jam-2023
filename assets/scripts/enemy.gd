4cb20a55ff69fb2c297fcbe63b0875913a336239
Lawrence <82725076+la-jig@users.noreply.github.com>
2023-08-08 18:50:54 -0400

    Working on enemys

--- assets/scripts/enemy.gd
+++ assets/scripts/enemy.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-@onready var sprite = $Sprite2D
-@onready var raycast = $RayCast2D
+@onready var sprite = $Texture
+
 
 @export var speed = 100
 
@@ -10,16 +10,19 @@ extends CharacterBody2D
 @export var position_1 = Vector2.ZERO
 @export var position_2 = Vector2.ZERO
 
+
+var current_waypoint = Vector2.ZERO
+
+
 var direction = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _ready():
+	current_waypoint = position_2
+
+
 func _physics_process(delta):
-	var motion := Vector2()
-	
-	if raycast.is_colliding():
-		change_direction()
-		
 	for i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(i)
 		if collision.normal.x > 0:
@@ -28,19 +31,20 @@ func _physics_process(delta):
 			return "right"
 		
 	if not is_on_floor():
-		motion.y += gravity * delta
+		velocity.y += gravity * delta
 	
-	motion.x = speed * direction
-	var collision := move_and_collide(motion)
+	
+	var collision = get_last_slide_collision()
 	
 	if collision and collision.get_collider():
 		var body = collision.get_collider()
 		
 		if body.is_in_group("player"):
 			body.hit()
+	
+	move_and_slide()
 
 
 func change_direction():
 	sprite.flip_h = !sprite.flip_h
 	direction *= -1
-	raycast.target_position *= Vector2(-1, 1)
